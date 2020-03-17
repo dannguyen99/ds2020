a316444b7a804cfa46cbd2f03285ae1e577ea7fb
danng <dannguyen0801@gmail.com>
2020-03-17 22:40:02 +0700

    add code of labwork2

---
+++ labwork2/src/FileServer.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.rmi.*;
+
+public class FileServer {
+    public static void main(String argv[]) {
+        if(System.getSecurityManager() == null) {
+            System.setSecurityManager(new RMISecurityManager());
+        }
+        try {
+            FileInterface fi = new FileImpl("policy.txt");
+            Naming.rebind("//127.0.0.1/FileServer", fi);
+        } catch(Exception e) {
+            System.out.println("FileServer: "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
